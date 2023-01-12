1aaf5f14d5acf5991729d24b10c38c72080a44dc
shaoxuan <shaoxuanhinhua@gmail.com>
2023-01-12 23:47:55 +0800

    add order and orders screen

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -3,11 +3,12 @@ import React from "react"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import TabNavigator from "./TabNavigator"
 import ModalScreen from "../screens/ModalScreen"
+import OrderScreen from "../screens/OrderScreen"
 
 export type RootStackParamList = {
   Main: undefined // no props for main screen
   MyModal: { userId: string; name: string }
-  Order: { order: any }
+  Order: { order: Order }
 }
 
 const RootStack = createNativeStackNavigator<RootStackParamList>()
@@ -29,6 +30,10 @@ const RootNavigator = () => {
           name="MyModal"
           component={ModalScreen}
         />
+      </RootStack.Group>
+
+      <RootStack.Group>
+        <RootStack.Screen name="Order" component={OrderScreen} />
       </RootStack.Group>
     </RootStack.Navigator>
   )
--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,11 +1,74 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { View, Text, ScrollView } from "react-native"
+import React, { useLayoutEffect, useState } from "react"
+import {
+  CompositeNavigationProp,
+  RouteProp,
+  useNavigation,
+} from "@react-navigation/native"
+import { RootStackParamList } from "../navigator/RootNavigator"
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs"
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { TabStackParamList } from "../navigator/TabNavigator"
+import { useTailwind } from "tailwind-rn/dist"
+import useOrders from "../hooks/useOrders"
+import { Button, Image } from "@rneui/themed"
+import Loader from "../components/Loader"
+import OrderCard from "../components/OrderCard"
+
+export type OrdersScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Orders">,
+  NativeStackNavigationProp<RootStackParamList>
+>
 
 const OrdersScreen = () => {
+  const tw = useTailwind()
+  const navigation = useNavigation<OrdersScreenNavigationProp>()
+
+  const { loading, error, orders } = useOrders()
+  const [ascending, setAscending] = useState<boolean>(false)
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      tabBarLabel: ({ focused, color }) => (
+        <Text style={{ color: focused ? "#EB6A7C" : color, fontSize: 10 }}>
+          Orders
+        </Text>
+      ),
+    })
+  }, [])
+
   return (
-    <View>
-      <Text>OrdersScreen</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: "#EB6A7C" }}>
+      <Image
+        source={{ uri: "https://source.unsplash.com/1cqIcrWFQBI" }}
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<Loader />}
+      />
+
+      <View>
+        <Button
+          color="pink"
+          titleStyle={{ color: "gray", fontWeight: "400" }}
+          style={tw("py-2 px-5")}
+          onPress={() => setAscending(!ascending)}
+        >
+          {ascending ? "Showing: Oldest First" : "Showing: Most Recent First"}
+        </Button>
+
+        {orders
+          ?.sort((a, b) => {
+            if (ascending) {
+              return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1
+            } else {
+              return new Date(a.createdAt) < new Date(b.createdAt) ? 1 : -1
+            }
+          })
+          .map((order) => (
+            <OrderCard key={order.trackingId} item={order} />
+          ))}
+      </View>
+    </ScrollView>
   )
 }
 
